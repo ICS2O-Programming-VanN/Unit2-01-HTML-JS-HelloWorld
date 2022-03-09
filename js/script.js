a019dc96b2adc8516224c4bde1045e4d34b81feb
Van Nguyen <99274250+van-nguyen2006@users.noreply.github.com>
2022-03-09 15:44:41 +0000

    Changed what "Click me" button displays

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function myButtonClicked () {
   // this function displays "Hello, World!"
   
-  document.getElementById('hello-world').innerHTML = 'I coded this in Javascript!'
+  document.getElementById('hello-world').innerHTML = 'You really clicked a button because it said so..?'
 }
