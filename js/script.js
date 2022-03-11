0272754b263428968fd9f00ffdfe1a6449803ca9
Van Nguyen <99274250+van-nguyen2006@users.noreply.github.com>
2022-03-11 05:24:26 +0000

    Added Comments.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function myButtonClicked () {
-  // this function displays "Hello, World!"
+  // this function displays "You really clicked a button because it said so..?"
   
   document.getElementById('hello-world').innerHTML = 'You really clicked a button because it said so..?'
 }
